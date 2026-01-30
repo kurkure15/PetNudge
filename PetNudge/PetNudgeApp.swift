5999b2b0c0b87e1bcd6ea69168c317581be82c06
Ankur Yadav <ankury15@gmail.com>
2026-01-30 18:44:20 +0400

    Initial Commit

---
+++ PetNudge/PetNudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetNudgeApp.swift
+//  PetNudge
+//
+//  Created by Ankur Yadav on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct PetNudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
